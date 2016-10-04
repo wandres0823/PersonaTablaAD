604f6d895a334785fb4bc171f42ed1c936311217
“william” <william200928@gmail>
2016-10-04 11:28:29 -0500

    interfaz terminada

---
+++ src/clases/Personas.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+/**
+ *
+ * @author walbonis1
+ */
+public class Personas {
+    private String cedula;
+    private String nombre;
+    private String apellido ;
+
+    public Personas(String cedula, String nombre, String apellido) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
